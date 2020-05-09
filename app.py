22a42b6be8d94c8572f613cef0b07f2330b0ac45
Matheus Zaché Gonçalves <matheuszache2@gmail.com>
2020-05-09 18:34:38 -0300

    *Teste*

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def teste():
     _relation["select"] = pd.Series(params["selecionado"]["Value"])
     
     staticPlots = sPlots.StaticPlots()
-    staticPlots.totalBarState(_relation['deaths'],_relation['ratio'])
+    staticPlots.totalBarCity(_relation['deaths'],_relation['ratio'])
 
     return 'Funcionou?'
 
